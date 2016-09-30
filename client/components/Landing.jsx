60863b1755ee2fdcf4643f372f6d57ffe2ad758e
Patrick Pan <patrickpan@college.harvard.edu>
2016-09-30 04:45:11 -0400

    Changed logo to avoid scaling issues

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Ghost from '../img/ghost.svg';
+import Ghost from '../img/ghost.png';
 import Snapcode from '../img/snapcode.png';
 
 class Landing extends React.Component {
@@ -30,16 +30,13 @@ class Landing extends React.Component {
         let centerImage = this.state.hover ?
             Snapcode :
             Ghost;
-        let centerImageClass = this.state.hover ?
-            "larger" :
-            "";
 
         return <div id="Landing"
             onMouseOver={onMouseOver}
             onMouseOut={onMouseOut}
         >
             <div id="landing-logo-container">
-                <img src={centerImage} className={centerImageClass} alt=""/>
+                <img src={centerImage} alt=""/>
             </div>
             <div id="landing-name" onClick={onClick}>Patrick Pan</div>
             <div id="landing-enter" onClick={onClick}>View my Resume</div>
