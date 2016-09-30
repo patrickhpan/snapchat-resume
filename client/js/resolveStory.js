82155544902228ab5be63e60727779b20c67ab45
Patrick Pan <patrickpan@college.harvard.edu>
2016-09-30 04:53:49 -0400

    Added mobile test

--- client/js/resolveStory.js
+++ client/js/resolveStory.js
@@ -1,4 +1,11 @@
+import isMobile from './isMobile'
+
 function resolveStory(story, file = "thumbnail.jpg") {
+    if(/mp4$/.test(file) && isMobile()) {
+        alert(
+            "Mobile"
+        )
+    }
     return `../media/${story}/${file}`.replace(/\s+/g, "_");
 }
 
